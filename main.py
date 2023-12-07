9f1407b38dbfe4b5a32a91406b7da5b2f2443609
AvenIsHere <128219717+AvenIsHere@users.noreply.github.com>
2023-12-07 17:38:38 +0000

    Added more menu options
    Added options, leaderboard, and quit option. Play button still does nothing, as does the leaderboard button. The option button takes you to the options menu where currently only fullscreen can be toggled. Quit button works perfectly.

--- main.py
+++ main.py
@@ -8,21 +8,40 @@ DISPLAYSURF = pygame.display.set_mode((400, 300), pygame.RESIZABLE)
 pygame.display.set_caption('NEA')
 font = pygame.font.Font(None, 32)
 
-menu = True
+menu = 'main'
 
-def button(text, position, size, colour, action=None):
+def button(text, position, size, colour, action=None, actionParameters=None):
   button_rect = pygame.Rect(position[0] - (size[0]/2), position[1] - (size[1]/2), size[0], size[1])
   pygame.draw.rect(DISPLAYSURF, colour, button_rect)
   text = font.render(text, True, (0, 0, 0))
   textRect = text.get_rect(center=button_rect.center)
   DISPLAYSURF.blit(text, textRect)
+  if button_rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] and action != None:
+    if actionParameters != None:
+      action(actionParameters)
+    else:
+      action()
 
-def mainMenu():
-  GameNameText = font.render("Game Name", True, (255, 255, 255))
-  GameNameTextRect = GameNameText.get_rect(center=(DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 6))
-  DISPLAYSURF.blit(GameNameText, GameNameTextRect)
-  button('Play', (GameNameTextRect.centerx, GameNameTextRect.centery + 100), (100, 50), (100, 100, 100))
-  
+def menuEquals(menu_set):
+  global menu
+  menu = menu_set
+
+def mainMenu(menu):
+  if menu == 'main':
+    menuNameText = font.render("Game Name", True, (255, 255, 255))
+  elif menu == 'settings':
+    menuNameText = font.render("Settings", True, (255, 255, 255))
+  menuNameTextRect = menuNameText.get_rect(center=(DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 6))
+  DISPLAYSURF.blit(menuNameText, menuNameTextRect)
+  if menu == 'main':
+    button('Play', (menuNameTextRect.centerx, menuNameTextRect.centery + 50), (150, 37.5), (100, 100, 100))
+    button('Settings', (menuNameTextRect.centerx, menuNameTextRect.centery + 100), (150, 37.5), (100, 100, 100), menuEquals, 'settings')
+    button('Leaderboard', (menuNameTextRect.centerx, menuNameTextRect.centery + 150), (150, 37.5), (100, 100, 100))
+    button('Quit', (menuNameTextRect.centerx, menuNameTextRect.centery + 200), (150, 37.5), (100, 100, 100), pygame.quit)
+  if menu == 'settings':
+    button('Fullscreen', (menuNameTextRect.centerx, menuNameTextRect.centery + 50), (150, 37.5), (100, 100, 100), pygame.display.toggle_fullscreen)
+    button('Back', (menuNameTextRect.centerx, menuNameTextRect.centery + 100), (150, 37.5), (100, 100, 100), menuEquals, 'main')
+
 
 while True:
   for event in pygame.event.get():
@@ -32,7 +51,6 @@ while True:
 
   DISPLAYSURF.fill((20,20,20))
 
-  if menu == True:
-    mainMenu()
+  mainMenu(menu)
   
   pygame.display.update() 
